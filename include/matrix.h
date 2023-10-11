9189b8bbe506d3c34c4a876d42228423e950fa92
Evgeniy Zaborshchikov <ezaborschikov@skala-r.ru>
2023-10-11 20:20:35 +0300

    formatt

--- include/matrix.h
+++ include/matrix.h
@@ -5,21 +5,22 @@
 #include <vector>
 
 class Matrix {
-   public:
+public:
     Matrix();
     Matrix(int num_rows, int num_cols);
-    Matrix(const Matrix &mat);
+    Matrix(const Matrix& mat);
 
     inline size_t getRows() const { return m_rows; }
     inline size_t getCols() const { return m_cols; }
-    inline int &at(size_t row, size_t col) { return m_data[row * m_cols + col]; }
+    inline int& at(size_t row, size_t col) { return m_data[row * m_cols + col]; }
 
     Matrix add(int val) const;
     Matrix multiply(int val) const;
     Matrix subtract(int val) const;
-    Matrix &operator=(const Matrix &mat);
+    Matrix& operator=(const Matrix& mat);
+    Matrix(const Matrix& mat); 
 
-   private:
+private:
     size_t m_rows;
     size_t m_cols;
     std::vector<int> m_data;
